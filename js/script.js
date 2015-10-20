c2a8d83b9dacf518f583114ecf1d97bbe4869097
TARANJYOTSINGH <tm05504n@pace.edu>
2015-10-20 13:56:00 -0400

    Applied the magic on hamburger , please try it

--- js/script.js
+++ js/script.js
@@ -2,36 +2,23 @@ $( document ).ready(function() {
     console.log( "ready!" );
 
 
-    // $("#work").css("text-decoration", "line-through");
-
-
-    $('.spinner-master2').click(function (){
-
-      if ($('.nav-links-responsive').is(':animated')) {
-        console.log('busy');
-      } else {
-
-        var clicks = $(this).data('clicks');
-        if (clicks){
-          $('.nav-links-responsive').slideDown('slow');
-          console.log("oneclick");
-        } else {
-          $('.nav-links-responsive').slideUp('fast');
-          console.log("twoclicks");
-        }
-        $(this).data('clicks', !clicks);
-      }
-    
-    });
-
    
 
-
-
-    // $("#load_about").on("click", function() {
-    //     $("#loader").load("about.html");
-    //     $(this).siblings().css("text-decoration", "none");
-    //     $(this).css("text-decoration", "line-through");
-    // });
+    $(".c-hamburger").click(function(e) {
+        $(".nav-links").toggleClass("menuOpen");
+        e.preventDefault();
+    });
+    var toggles = document.querySelectorAll(".c-hamburger");
+    for (var i = toggles.length - 1; i >= 0; i--) {
+      var toggle = toggles[i];
+      toggleHandler(toggle);
+    };
+    function toggleHandler(toggle) {
+      toggle.addEventListener( "click", function(e) {
+        e.preventDefault();
+        (this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
+      });
+    }
+    
 
 });
